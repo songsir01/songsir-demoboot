2b5adc37e74c058f50d5e06c5c2532703d017a68
songyapeng <songyapeng@shopin.cn>
2018-12-03 14:23:41 +0800

    tt

---
+++ mysqlDB/student.sql
@@ -0,0 +1,39 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : songsir
+Source Server Version : 50552
+Source Host           : localhost:3306
+Source Database       : songsirsdb
+
+Target Server Type    : MYSQL
+Target Server Version : 50552
+File Encoding         : 65001
+
+Date: 2018-11-28 17:38:56
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `SID` varchar(10) DEFAULT NULL,
+  `Sname` varchar(10) DEFAULT NULL,
+  `Sage` datetime DEFAULT NULL,
+  `Ssex` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES ('01', '赵雷', '1990-01-01 00:00:00', '男');
+INSERT INTO `student` VALUES ('02', '钱电', '1990-12-21 00:00:00', '男');
+INSERT INTO `student` VALUES ('03', '孙风', '1990-05-20 00:00:00', '男');
+INSERT INTO `student` VALUES ('04', '李云', '1990-08-06 00:00:00', '男');
+INSERT INTO `student` VALUES ('05', '周梅', '1991-12-01 00:00:00', '女');
+INSERT INTO `student` VALUES ('06', '吴兰', '1992-03-01 00:00:00', '女');
+INSERT INTO `student` VALUES ('07', '郑竹', '1989-07-01 00:00:00', '女');
+INSERT INTO `student` VALUES ('08', '王菊', '1990-01-20 00:00:00', '女');
