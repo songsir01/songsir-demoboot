4f7bcb456a02be9e95309e9012901c1a8e66ab56
songyapeng <songsir01@163.com>
2019-06-14 09:12:05 +0800

    test 第二次提交

---
+++ songsir-commont/src/java/com/songsir/test/TestMain.java
@@ -0,0 +1,20 @@
+package com.songsir.test;
+
+import org.junit.Test;
+
+/**
+ * @PackageName com.songsir.test
+ * @ProjectName songsir-demoboot
+ * @Auther: SongYapeng
+ * @Date: Create in 9:10 2019/6/14
+ * @Description:
+ * @Copyright Copyright (c) 2019, songyapeng@shopin.cn All Rights Reserved.
+ */
+public class TestMain {
+
+
+    @Test
+    public void test2() {
+
+    }
+}
