4f591163e051a2aa7fe3af7cb78cbb2ad3d4804b
songsir01 <1126563633@qq.com>
2019-07-09 15:41:32 +0800

    定时任务简单实现。。。

---
+++ songsir-web/src/main/java/com/songsir/schedule/SongsriTestJob.java
@@ -0,0 +1,31 @@
+package com.songsir.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * @PackageName com.songsir.schedule
+ * @ProjectName songsir-demoboot
+ * @Author: SongYapeng
+ * @Date: Create in 15:29 2019/7/9
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+@Configuration
+@EnableScheduling
+@PropertySource(value = "classpath:/schedule.properties")
+public class SongsriTestJob {
+
+    private static Logger logger = LoggerFactory.getLogger(SongsriTestJob.class);
+
+
+    @Scheduled(cron = "${songsirTestJob}")
+    public void work() {
+        logger.info("the task is running....");
+    }
+
+}
