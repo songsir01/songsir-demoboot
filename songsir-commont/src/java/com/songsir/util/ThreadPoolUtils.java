8ec5c918ffbeea41ecbf7d8bf62b2294e66dadd6
songsir01 <1126563633@qq.com>
2019-08-06 17:20:32 +0800

    增加 线程池创建 工具类

---
+++ songsir-commont/src/java/com/songsir/util/ThreadPoolUtils.java
@@ -0,0 +1,38 @@
+package com.songsir.util;
+
+import java.util.concurrent.*;
+
+/**
+ * @PackageName com.songsir.util
+ * @ProjectName songsir-demoboot
+ * @Author: SongYapeng
+ * @Date: Create in 17:12 2019/8/6
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+public class ThreadPoolUtils {
+
+    /**
+     * @param poolSize
+     * @param method
+     * @MethodName creatExeutorService
+     * @Description 创建线程池方法
+     * @Auther SongYapeng
+     * @Date 2019/8/6 17:19
+     * @Since JDK 1.8
+     */
+    private ExecutorService creatExeutorService(int poolSize, String method) {
+        int coreSize = Runtime.getRuntime().availableProcessors();
+        if (poolSize < coreSize) {
+            coreSize = poolSize;
+        }
+        ThreadFactory threadFactory = r -> {
+            Thread t = new Thread(r, "thread created mewthod{" + method + "}");
+            t.setDaemon(true);
+            return t;
+        };
+        BlockingQueue<Runnable> queueToUse = new LinkedBlockingQueue<>(coreSize);
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(coreSize, poolSize, 60, TimeUnit.SECONDS, queueToUse, threadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
+    }
+}
