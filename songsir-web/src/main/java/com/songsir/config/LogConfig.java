378a608dcea4fc749a8a2b49689435b7317bfb81
songsir01 <1126563633@qq.com>
2019-08-06 16:28:40 +0800

    remove unuse import

--- songsir-web/src/main/java/com/songsir/config/LogConfig.java
+++ songsir-web/src/main/java/com/songsir/config/LogConfig.java
@@ -1,6 +1,5 @@
 package com.songsir.config;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
