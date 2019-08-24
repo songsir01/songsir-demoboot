99fbb6c884db5795ad327c01c68c3dbc16429a9b
songsir01 <1126563633@qq.com>
2019-08-24 10:34:27 +0800

    update springboot version to 2.0.5

--- songsir-web/src/main/java/com/songsir/SongsirApplication.java
+++ songsir-web/src/main/java/com/songsir/SongsirApplication.java
@@ -3,11 +3,10 @@ package com.songsir;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @PackageName com.songsir
