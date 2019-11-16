d89ce48c7251bb6f7817f27177d8d4b018dd18a9
songsir01 <1126563633@qq.com>
2019-11-16 19:28:25 +0800

    add 联合索引最左匹配原则

--- mysqlDB/DDL/联合索引测试/test_table.sql
+++ mysqlDB/DDL/联合索引测试/test_table.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50552
 File Encoding         : 65001
 
-Date: 2019-11-08 16:53:38
+Date: 2019-11-16 19:03:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,14 +20,14 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `test_table`;
 CREATE TABLE `test_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `namee` varchar(255) DEFAULT NULL,
-  `sex` varchar(255) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `bitthday` varchar(255) DEFAULT NULL,
-  `aihao` varchar(255) DEFAULT NULL,
-  `xuehao` varchar(255) DEFAULT NULL,
-  `mingci` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `namee` varchar(255) DEFAULT NULL COMMENT '姓名',
+  `sex` varchar(255) DEFAULT NULL COMMENT '性别',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `bitthday` varchar(255) DEFAULT NULL COMMENT '生日',
+  `aihao` varchar(255) DEFAULT NULL COMMENT '爱好',
+  `xuehao` varchar(255) DEFAULT NULL COMMENT '学号',
+  `mingci` int(11) DEFAULT NULL COMMENT '名词',
   PRIMARY KEY (`id`),
   KEY `lianhe` (`namee`,`xuehao`,`mingci`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5002 DEFAULT CHARSET=utf8;
