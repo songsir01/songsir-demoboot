025b9f9435026647c150dbb4ff30eed7e18f3642
songsir01 <1126563633@qq.com>
2020-02-24 18:45:51 +0800

    添加计数demo

--- songsir-highly-concurrent/src/main/java/com/songsir/CountExample.java
+++ songsir-highly-concurrent/src/main/java/com/songsir/CountExample.java
@@ -1,5 +1,12 @@
 package com.songsir;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 /**
  * @PackageName com.songsir
  * @ProjectName songsir-demoboot
@@ -10,5 +17,33 @@ package com.songsir;
  */
 public class CountExample {
 
+    private static Logger logger = LoggerFactory.getLogger(CountExample.class);
+
+    private static int threadTotal = 200;
+    private static int clientTotal = 5000;
+    private static long count = 0;
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        for (int index = 0; index < clientTotal; index++) {
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    logger.error("Exception", e);
+                }
+            });
+        }
+        exec.shutdown();
+        logger.info("count: {}", count);
+    }
+
+    private static void add() {
+        count++;
+    }
 
 }
