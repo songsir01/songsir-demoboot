11ad3065240a1a10afc498600ec4b2d5cfe2c12b
songsir01 <1126563633@qq.com>
2020-02-26 20:28:33 +0800

    AtomitBoolean

---
+++ songsir-highly-concurrent/src/main/java/com/songsir/concurrency/ConcurrencyTest6.java
@@ -0,0 +1,57 @@
+package com.songsir.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @PackageName com.songsir.concurrency
+ * @ProjectName songsir-demoboot
+ * @Author: SongYapeng
+ * @Date: Create in 20:36 2020/2/25
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+@Slf4j
+public class ConcurrencyTest6 {
+
+    private static AtomicBoolean isHappened = new AtomicBoolean(false);
+
+    public static int clientTotal = 5000;
+
+    public static int threadTatal = 50;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Semaphore semaphore = new Semaphore(threadTatal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    test();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("excuption", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("isHappened: {}", isHappened.get());
+    }
+
+    private static void test() {
+        if (isHappened.compareAndSet(false, true)) {
+            log.info("execute");
+        }
+    }
+
+
+}
